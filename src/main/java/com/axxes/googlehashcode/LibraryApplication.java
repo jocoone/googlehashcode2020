7c88e9e9136782828b0c8da92a8843c2dbac27eb
Arne Hendrickx <arne.hendrickx.05@gmail.com>
2020-02-20 19:56:59 +0100

    it works

--- src/main/java/com/axxes/googlehashcode/LibraryApplication.java
+++ src/main/java/com/axxes/googlehashcode/LibraryApplication.java
@@ -9,11 +9,20 @@ import java.util.stream.*;
 import static com.axxes.googlehashcode.util.Util.*;
 
 public class LibraryApplication {
-	public static final String filename = "a_example";
+	//	public static final String filename = "a_example";
+	public static final String filename = "b_read_on";
 	public static final String file = "src/main/resources/" + filename + ".txt";
-	private static final String output_file = "a_example_output";
+	//	private static final String output_file = "a_example_output";
+	private static final String output_file = "b_read_on_output";
+	private static final String newLine = "\n";
 
 	public static void main(String[] args) {
+		List<Library> libraries = convert();
+
+		createOutput(output_file, libraries);
+	}
+
+	private static List<Library> convert() {
 		final List<String> lines = readLines(Paths.get(file)
 												  .toString(), 0);
 		String[] firstLine = lines.get(0)
@@ -27,7 +36,7 @@ public class LibraryApplication {
 			books[i] = Integer.parseInt(second[i]);
 		}
 		int indexLib = 0;
-		for (int i = 1; i < lines.size() - 1; i++) {
+		for (int i = 2; i < lines.size() - 1; i += 2) {
 			String[] libraryProperties = lines.get(i)
 											  .split(" ");
 			int amountOfBooks = Integer.parseInt(libraryProperties[0]);
@@ -47,19 +56,19 @@ public class LibraryApplication {
 			libraries.add(lib);
 			indexLib++;
 		}
-
-		createOutput(output_file, libraries);
+		return libraries;
 	}
 
 	public static void createOutput(String fileName, List<Library> libraries) {
-		final StringBuilder builder = new StringBuilder("" + libraries.size());
+		final StringBuilder builder = new StringBuilder("" + libraries.size()).append(newLine);
 		libraries.forEach(lib -> {
 			builder.append(lib.id)
 				   .append(" ")
 				   .append(lib.books.size())
-				   .append("\n");
+				   .append(newLine);
 			lib.books.forEach(b -> builder.append(b)
 										  .append(" "));
+			builder.append(newLine);
 		});
 		writeString(fileName, builder.toString());
 	}
