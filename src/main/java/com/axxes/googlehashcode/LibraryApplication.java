107f41a969b944fa25a2e6cac0826f3305409857
Arne Hendrickx <arne.hendrickx.05@gmail.com>
2020-02-20 19:47:55 +0100

    temp 3

--- src/main/java/com/axxes/googlehashcode/LibraryApplication.java
+++ src/main/java/com/axxes/googlehashcode/LibraryApplication.java
@@ -3,7 +3,7 @@ package com.axxes.googlehashcode;
 import com.axxes.googlehashcode.model.Library;
 
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.*;
 import java.util.stream.*;
 
 import static com.axxes.googlehashcode.util.Util.*;
@@ -11,14 +11,15 @@ import static com.axxes.googlehashcode.util.Util.*;
 public class LibraryApplication {
 	public static final String filename = "a_example";
 	public static final String file = "src/main/resources/" + filename + ".txt";
+	private static final String output_file = "a_example_output";
 
 	public static void main(String[] args) {
 		final List<String> lines = readLines(Paths.get(file)
 												  .toString(), 0);
 		String[] firstLine = lines.get(0)
 								  .split(" ");
-		Library[] libraries = new Library[Integer.parseInt(firstLine[1])];
-		int days = Integer.parseInt(firstLine[3]);
+		List<Library> libraries = new ArrayList<>();
+		int days = Integer.parseInt(firstLine[2]);
 		String[] second = lines.get(1)
 							   .split(" ");
 		int books[] = new int[second.length];
@@ -26,14 +27,14 @@ public class LibraryApplication {
 			books[i] = Integer.parseInt(second[i]);
 		}
 		int indexLib = 0;
-		for (int i = 2; i < lines.size() - 1; i++) {
+		for (int i = 1; i < lines.size() - 1; i++) {
 			String[] libraryProperties = lines.get(i)
 											  .split(" ");
 			int amountOfBooks = Integer.parseInt(libraryProperties[0]);
 			int processedDays = Integer.parseInt(libraryProperties[1]);
 			int signupDays = Integer.parseInt(libraryProperties[2]);
 
-			Library lib = libraries[indexLib];
+			Library lib = new Library();
 
 			lib.bookPerDay = processedDays;
 			lib.signUp = signupDays;
@@ -42,8 +43,12 @@ public class LibraryApplication {
 									   .split(" "))
 							  .map(Integer::parseInt)
 							  .collect(Collectors.toList());
+
+			libraries.add(lib);
 			indexLib++;
 		}
+
+		createOutput(output_file, libraries);
 	}
 
 	public static void createOutput(String fileName, List<Library> libraries) {
